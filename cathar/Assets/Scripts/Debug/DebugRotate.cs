4cf11bf30067162eaab42ee36d652abe09300d21
Pierre-Alexandre Leclerc <pierrealexandre.leclerc@gmail.com>
2015-08-24 20:49:26 -0400

    Setup Project

---
+++ cathar/Assets/Scripts/Debug/DebugRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugRotate : MonoBehaviour {
+
+	public Vector3 m_RotateToApply;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (m_RotateToApply);
+	}
+}
