c0f313b11794a60560deed3af3b11dcd812e42c4
Pierre-Alexandre Leclerc <pierrealexandre.leclerc@gmail.com>
2015-08-27 20:59:40 -0400

    Speed in function of the controller stick

--- cathar/Assets/Scripts/Debug/PlayerMovements.cs
+++ cathar/Assets/Scripts/Debug/PlayerMovements.cs
@@ -13,9 +13,13 @@ public class PlayerMovements : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		Vector2 velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal"), 0.8f),
-		                                   Mathf.Lerp(0, Input.GetAxis("Vertical"), 0.8f));
+		float axisX = Input.GetAxis ("Horizontal");
+		float axisY = Input.GetAxis("Vertical");
 
-		m_RigidBody.velocity = velocity.normalized * m_Speed;
+		Vector2 velocity = new Vector2(Mathf.Lerp(0, axisX, 0.8f),
+		                               Mathf.Lerp(0, axisY, 0.8f));
+
+		float stickSpeed = Mathf.Max (Mathf.Abs (axisX), Mathf.Abs(axisY));
+		m_RigidBody.velocity = velocity.normalized * m_Speed*stickSpeed;
 	}
 }
