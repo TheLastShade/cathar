091843b2876c7b6b75c9afa75e77dbd7d86bb751
Pierre-Alexandre Leclerc <pierrealexandre.leclerc@gmail.com>
2015-08-27 22:00:59 -0400

    Add camera follow & quick fix for error spam at animation idle
    Better fix for the error soon

---
+++ cathar/Assets/Scripts/Debug/Follow2DTransform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow2DTransform : MonoBehaviour {
+
+	public Transform m_ToFollow;
+	private Transform m_MyTransform;
+	// Use this for initialization
+	void Start () {
+		m_MyTransform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 newPosition = new Vector2 (m_ToFollow.position.x, m_ToFollow.position.y);
+		m_MyTransform.position = newPosition;
+	}
+}
