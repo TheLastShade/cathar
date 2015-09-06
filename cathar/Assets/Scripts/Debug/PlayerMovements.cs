710b9d172388d0491a3710fb25d8fcfe8af5e03b
orymus <orymus@gmail.com>
2015-09-05 21:21:42 -0400

    Tweaked TerrainModifier to include speedModifier for ice/oil

--- cathar/Assets/Scripts/Debug/PlayerMovements.cs
+++ cathar/Assets/Scripts/Debug/PlayerMovements.cs
@@ -5,6 +5,7 @@ public class PlayerMovements : MonoBehaviour {
 
 	public float m_Speed;
 	public float defaultDrag;
+	private float currentSpeed;
 
 	private Rigidbody2D m_RigidBody;
 
@@ -12,6 +13,7 @@ public class PlayerMovements : MonoBehaviour {
 	void Start () {
 		m_RigidBody = GetComponent<Rigidbody2D> ();
 		m_RigidBody.drag = defaultDrag;
+		currentSpeed = m_Speed;
 	}
 
 	void FixedUpdate () 
@@ -23,22 +25,30 @@ public class PlayerMovements : MonoBehaviour {
 		                               Mathf.Lerp(0, axisY, 0.8f));
 
 		float stickSpeed = Mathf.Max (Mathf.Abs (axisX), Mathf.Abs(axisY));
-		//m_RigidBody.velocity = velocity.normalized * m_Speed*stickSpeed;
-		m_RigidBody.AddForce (velocity.normalized * m_Speed * stickSpeed);
+		//m_RigidBody.velocity = velocity.normalized * currentSpeed*stickSpeed;
+		m_RigidBody.AddForce (velocity.normalized * currentSpeed * stickSpeed);
 	}
+
 
 	void OnTriggerEnter2D(Collider2D aCollider){
 		TerrainModifier modifier = aCollider.GetComponent<TerrainModifier> ();
 
 		if (modifier != null) {
 			m_RigidBody.drag = modifier.dragModifier;
+
+			if (modifier.speedModifier != 0){
+				currentSpeed = modifier.speedModifier;
+			}
+
 		}
 	}
+
 
 	void OnTriggerExit2D (Collider2D aCollider){
 		TerrainModifier modifier = aCollider.GetComponent<TerrainModifier> ();
 		if (modifier != null) {
 			m_RigidBody.drag = defaultDrag;
+			currentSpeed = m_Speed;
 		}
 	}
 
--- cathar/Assets/Scripts/Debug/TerrainModifier.cs
+++ cathar/Assets/Scripts/Debug/TerrainModifier.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public class TerrainModifier : MonoBehaviour {
 	
 	public float dragModifier;
+	public float speedModifier;
 
 }
