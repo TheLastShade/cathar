794400f21fdd9dd2607540721b45af2584d89a7b
orymus <orymus@gmail.com>
2015-09-05 21:15:58 -0400

    Simplified and polished Draf modifiers for terrain.

--- cathar/Assets/Scripts/Debug/PlayerMovements.cs
+++ cathar/Assets/Scripts/Debug/PlayerMovements.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class PlayerMovements : MonoBehaviour {
 
 	public float m_Speed;
+	public float defaultDrag;
+
 	private Rigidbody2D m_RigidBody;
-	// Use this for initialization
+
+
 	void Start () {
 		m_RigidBody = GetComponent<Rigidbody2D> ();
+		m_RigidBody.drag = defaultDrag;
 	}
-	
-	// Update is called once per frame
+
 	void FixedUpdate () 
 	{
 		float axisX = Input.GetAxis (ControllerSave.m_HorizontalAxis);
@@ -20,6 +23,23 @@ public class PlayerMovements : MonoBehaviour {
 		                               Mathf.Lerp(0, axisY, 0.8f));
 
 		float stickSpeed = Mathf.Max (Mathf.Abs (axisX), Mathf.Abs(axisY));
-		m_RigidBody.velocity = velocity.normalized * m_Speed*stickSpeed;
+		//m_RigidBody.velocity = velocity.normalized * m_Speed*stickSpeed;
+		m_RigidBody.AddForce (velocity.normalized * m_Speed * stickSpeed);
 	}
+
+	void OnTriggerEnter2D(Collider2D aCollider){
+		TerrainModifier modifier = aCollider.GetComponent<TerrainModifier> ();
+
+		if (modifier != null) {
+			m_RigidBody.drag = modifier.dragModifier;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D aCollider){
+		TerrainModifier modifier = aCollider.GetComponent<TerrainModifier> ();
+		if (modifier != null) {
+			m_RigidBody.drag = defaultDrag;
+		}
+	}
+
 }
---
+++ cathar/Assets/Scripts/Debug/TerrainModifier.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainModifier : MonoBehaviour {
+	
+	public float dragModifier;
+
+}
