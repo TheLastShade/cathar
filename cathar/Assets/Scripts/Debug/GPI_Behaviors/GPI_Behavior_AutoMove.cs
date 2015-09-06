c314e1ae6fa36f8fe8dee0047b975eee2196c9f7
orymus <orymus@gmail.com>
2015-09-05 21:52:35 -0400

    Added AutoMove GPI Behavior

---
+++ cathar/Assets/Scripts/Debug/GPI_Behaviors/GPI_Behavior_AutoMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPI_Behavior_AutoMove : MonoBehaviour {
+
+	public bool isHorizontal;
+	public int speed;
+	private Rigidbody2D body;
+
+	void Start(){
+		body = GetComponent<Rigidbody2D> ();
+		AdjustSpeed ();
+	}
+
+	void Update(){
+		print (body.velocity.magnitude);
+	}
+
+	void OnCollisionEnter2D (Collision2D aCollision){
+		speed = -speed;
+		AdjustSpeed ();
+
+	}
+
+
+	void AdjustSpeed(){
+		if (isHorizontal) {
+			body.velocity = (new Vector2 (speed, 0));
+		} else {
+			body.velocity = (new Vector2 (0,speed));
+		}
+	}
+
+}
