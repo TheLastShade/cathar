c0a28e4d346f1aaa37144e852a15edeee7560c74
orymus <orymus@gmail.com>
2015-09-06 22:18:56 -0400

    Modified Spitter + simple rotation speed
    + Spitter: added MuzzleFlare effect (temp) and added contingency for
    missing refs.
    + Rotation speed script

--- cathar/Assets/GPI_Behavior_Spitter.cs
+++ cathar/Assets/GPI_Behavior_Spitter.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GPI_Behavior_Spitter : MonoBehaviour {
 
 	public GameObject projectile;
+	public GameObject muzzleFlare;
 	public float cooldown;
 	public float speed;
 	int currentCooldown;
@@ -13,12 +14,28 @@ public class GPI_Behavior_Spitter : MonoBehaviour {
 
 		if (currentCooldown >= cooldown) {
 			currentCooldown = 0;
-			GameObject newProjectile = Instantiate(projectile);
-			newProjectile.transform.position = this.transform.position + this.transform.up * 0.75f;
-			newProjectile.transform.rotation = this.transform.rotation;
-			Rigidbody2D newBody = newProjectile.GetComponent<Rigidbody2D>();
-			newBody.AddForce (newBody.transform.up * speed);
-			//newProjectile.transform.position = new Vector3 (newProjectile.transform.position.x, newProjectile.transform.position.y, 50);
+
+			if (muzzleFlare != null){
+				GameObject newMuzzleFlare = Instantiate(muzzleFlare);
+				Animator anim = newMuzzleFlare.GetComponent<Animator>();
+				anim.speed = 3f;
+				newMuzzleFlare.transform.localScale = new Vector3 (0.25f, 1.5f, 1f);
+				newMuzzleFlare.transform.rotation = this.transform.rotation;
+				newMuzzleFlare.transform.position = this.transform.position + (this.transform.up * 0.6f);
+				newMuzzleFlare.transform.position = new Vector3 (newMuzzleFlare.transform.position.x, newMuzzleFlare.transform.position.y, 40);
+			}
+
+			if (projectile != null)
+			{
+				GameObject newProjectile = Instantiate(projectile);
+				SpriteRenderer sr = newProjectile.GetComponent<SpriteRenderer>();
+				newProjectile.transform.position = this.transform.position + this.transform.up * (sr.bounds.extents.magnitude * 3f);
+				newProjectile.transform.rotation = this.transform.rotation;
+				Rigidbody2D newBody = newProjectile.GetComponent<Rigidbody2D>();
+				newBody.AddForce (newBody.transform.up * speed);
+				//newProjectile.transform.position = new Vector3 (newProjectile.transform.position.x, newProjectile.transform.position.y, 50);
+			}
+
 		}
 	}
 }
---
+++ cathar/Assets/Scripts/Debug/GPI_Behaviors/GPI_Behavior_Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPI_Behavior_Rotate : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		this.transform.Rotate (new Vector3 (0,0,speed));
+	}
+}
